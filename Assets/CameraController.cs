060a4b210bdb2c0456a338de5639b764d416a20f
John <jofouts@gmail.com>
2019-07-24 09:56:22 -0700

    first commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Adds a slight lag to camera rotation to make the third person camera a little more interesting.
+/// Requires that it starts parented to something in order to follow it correctly.
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    Vector2 rotation = new Vector2(0, 0);
+    public float speed = 3;
+
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
+    }
+}
---
+++ Assets/Spaceship.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    Rigidbody rb;
+    GameObject CameraGameObject;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+        CameraGameObject = transform.Find("MainCamera").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForceAtPosition(transform.forward * 1.2f, transform.position);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForceAtPosition(-1 * transform.forward * 1.2f, transform.position);
+        }
+       
+
+        //put the camera behind the gameobject
+        CameraGameObject.transform.position = Vector3.Lerp(CameraGameObject.transform.position, transform.position + (transform.up * 0.8f) + (-3 * CameraGameObject.transform.forward.normalized), Time.deltaTime * 7);
+        //user force to rotate the ship towards the cameras vector
+        transform.forward = Vector3.Lerp(transform.forward, CameraGameObject.transform.forward, Time.deltaTime);
+        //shoot a bullet
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject newBullet = GameObject.Instantiate(bullet, transform.position, Quaternion.identity);
+            newBullet.GetComponent<Rigidbody>().AddForce(transform.forward*500);
+            GameObject.Destroy(newBullet, 10);
+        }
+    }
+}
